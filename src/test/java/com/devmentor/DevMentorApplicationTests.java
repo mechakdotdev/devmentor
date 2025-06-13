3e8072fd8dd3112011ef2fa14e21b6bb59d5b485
Mechak Holondo <mechakand1@gmail.com>
2025-06-13 10:18:21 +0100

    git init

---
+++ src/test/java/com/devmentor/DevMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMentorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
